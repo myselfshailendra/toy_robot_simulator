7582826376f51cbf75a8f5611731e46b262c4f51
Shailendra Prajapati <myselfshailendra@hotmail.com>
2017-12-17 18:12:48 +0530

    Created robot instance in simulator constructor

--- lib/Simulator.rb
+++ lib/Simulator.rb
@@ -1,3 +1,9 @@
 class Simulator
 
+  attr_reader :robot
+
+  def initialize
+    @robot = Robot.new
+  end
+
 end
--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -1,13 +1,17 @@
 require 'rspec'
 require './lib/simulator'
+require './lib/surface'
+require './lib/robot'
 
 describe Simulator do
 
   let(:simulator) { Simulator.new }
+  let(:robot) { simulator.robot }
 
   describe '#new' do
-    it 'initialize new instance for simulator' do
+    it 'initialize new instances for simulator and robot' do
       expect(simulator).to be_a Simulator
+      expect(robot).to be_a Robot
     end
   end
 
