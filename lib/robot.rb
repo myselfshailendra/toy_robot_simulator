9651ac4b969b3247038d7b10a8aabcfba055d747
Shailendra Prajapati <myselfshailendra@hotmail.com>
2017-12-17 14:54:34 +0530

    Added Place method to place Robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,25 @@
 class Robot
 
-  attr_reader :surface
+  attr_accessor :surface, :x_position, :y_position, :facing
+  DIRECTIONS = [:NORTH, :WEST, :SOUTH, :EAST]
 
   def initialize
     @surface = Surface.new
+  end
+
+  def place(x_position, y_position, facing)
+    if @surface.valid_position?(x_position, y_position) && valid_facing?(facing)
+      self.x_position = x_position
+      self.y_position = y_position
+      self.facing = facing
+    end
+  end
+
+  private
+
+  def valid_facing?(facing)
+    return true if DIRECTIONS.include?(facing)
+    raise StandardError, "`#{facing}` is not belongs to valid facing directions [NORTH, EAST, SOUTH and WEST]"
   end
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -14,4 +14,29 @@ describe Robot do
     end
   end
 
+  describe '#place' do
+    context 'when position is not valid' do
+      it { expect{robot.place(-1,0,:NORTH)}.to raise_error(StandardError, "Position is not valid!") }
+      it { expect{robot.place(6,6,:WEST)}.to raise_error(StandardError, "Position is not valid!") }
+    end
+    context 'when facing is not valid' do
+      it { expect{robot.place(2,3,:FIFTH)}.to raise_error(StandardError, "`FIFTH` is not belongs to valid facing directions [NORTH, EAST, SOUTH and WEST]") }
+      it { expect{robot.place(1,4,:XYZ)}.to raise_error(StandardError, "`XYZ` is not belongs to valid facing directions [NORTH, EAST, SOUTH and WEST]") }
+    end
+    context 'when position and facing both are valid' do
+      it 'place robot on position 1,2,NORTH' do
+        robot.place(1,2,:NORTH)
+        expect(robot.x_position).to eq 1
+        expect(robot.y_position).to eq 2
+        expect(robot.facing).to eq :NORTH
+      end
+      it 'place robot on position 3,4,WEST' do
+        robot.place(3,4,:WEST)
+        expect(robot.x_position).to eq 3
+        expect(robot.y_position).to eq 4
+        expect(robot.facing).to eq :WEST
+      end
+    end
+  end
+
 end
